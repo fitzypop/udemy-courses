41a7be959d44b46094242d5f627099ddd5ad55ab
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2021-04-27 08:26:14 -0600

    finished first assignment in section 2

--- js-complete-udemy/section_2_basics/basics-01-starting-project/assets/scripts/app.js
+++ js-complete-udemy/section_2_basics/basics-01-starting-project/assets/scripts/app.js
@@ -1,1 +1,11 @@
-alert('This works!');
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = ((currentResult + 10) * 3) / 2 - 1;
+
+let calcDescription = `(${defaultResult} + 10) * 3 / 2 -1`;
+outputResult(currentResult, calcDescription);
---
+++ js-complete-udemy/section_2_basics/basics-assignment-1-start/assignment.js
@@ -0,0 +1,9 @@
+const input = 42;
+let result;
+
+result = input + 18;
+result -= 21;
+result *= 13;
+result /= 2;
+alert(`Value of input: ${input}`);
+alert(`Value of result: ${result}`);
