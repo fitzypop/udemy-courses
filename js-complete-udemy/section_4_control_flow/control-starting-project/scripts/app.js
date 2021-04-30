f11e24ba7e401a03f4ceba1b323119a03c9b794f
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2021-04-29 18:03:32 -0600

    working through section 4, added monster killer project from instructors given code

--- js-complete-udemy/section_4_control_flow/control-starting-project/scripts/app.js
+++ js-complete-udemy/section_4_control_flow/control-starting-project/scripts/app.js
@@ -2,80 +2,90 @@ const defaultResult = 0;
 let currentResult = defaultResult;
 let logEntries = [];
 
-const usrInput = document.getElementById("input-number");
-const addBtn = document.getElementById("btn-add");
-const subtractBtn = document.getElementById("btn-subtract");
-const multiplyBtn = document.getElementById("btn-multiply");
-const divideBtn = document.getElementById("btn-divide");
+const Operations = {
+  Add: {
+    Name: "ADD",
+    Operator: "+",
+  },
+  Subtract: {
+    Name: "SUBTRACT",
+    Operator: "-",
+  },
+  Multiple: {
+    Name: "MULTIPLY",
+    Operator: "*",
+  },
+  Divide: {
+    Name: "DIVIDE",
+    Operator: "/",
+  },
+};
 
+// Get input/output elements
+const userInput = document.getElementById("input-number");
 const currentResultOutput = document.getElementById("current-result");
 const currentCalculationOutput = document.getElementById("current-calculation");
 
-function outputResult(result, text) {
-  currentResultOutput.textContent = result;
-  currentCalculationOutput.textContent = text;
+// Generates and writes calculation log
+function logAndWriteOutput(operation, initalResult, inputNum) {
+  logEntries.push({
+    operation: operation.Name,
+    prevResult: initalResult,
+    number: inputNum,
+    result: currentResult,
+  });
+  console.log(logEntries);
+
+  currentResultOutput.textContent = currentResult;
+  currentCalculationOutput.textContent = `${initalResult} ${operation.Operator} ${inputNum}`;
 }
 
 // Gets input from input field
 function getUserNumberInput() {
-  return parseInt(usrInput.value);
+  const input = +userInput.value;
+  if (Number.isNaN(input)) {
+    throw new Error('Invalid Input from "input-number" field.');
+  }
+
+  return input;
 }
 
-// Generates and writes calculation log
-function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
-  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
-  outputResult(currentResult, calcDescription); // from vendor file
-}
-
-function writeToLog(
-  operationIdentifier,
-  prevResult,
-  operationNumber,
-  newResult
-) {
-  const logEntry = {
-    operation: operationIdentifier,
-    prevResult: prevResult,
-    number: operationNumber,
-    result: newResult,
-  };
-  logEntries.push(logEntry);
-  console.log(logEntries);
-}
-
-function add() {
+function calculate(operation) {
   const enteredNumber = getUserNumberInput();
+  if (!enteredNumber) {
+    return;
+  }
+
   const initialResult = currentResult;
-  currentResult += enteredNumber;
-  createAndWriteOutput("+", initialResult, enteredNumber);
-  writeToLog("ADD", initialResult, enteredNumber, currentResult);
+
+  switch (operation) {
+    case Operations.Add:
+      currentResult += enteredNumber;
+      break;
+    case Operations.Subtract:
+      currentResult -= enteredNumber;
+      break;
+    case Operations.Multiple:
+      currentResult *= enteredNumber;
+      break;
+    case Operations.Divide:
+      currentResult /= enteredNumber;
+      break;
+    default:
+      throw new Error(`Unsupported Opertaion: ${calculationType}`);
+  }
+
+  logAndWriteOutput(operation, initialResult, enteredNumber);
 }
 
-function subtract() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult -= enteredNumber;
-  createAndWriteOutput("-", initialResult, enteredNumber);
-  writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult);
-}
+// Add onclick events to buttons
+document.getElementById("btn-add").onclick = () => calculate(Operations.Add);
 
-function multiply() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult *= enteredNumber;
-  createAndWriteOutput("*", initialResult, enteredNumber);
-  writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult);
-}
+document.getElementById("btn-subtract").onclick = () =>
+  calculate(Operations.Subtract);
 
-function divide() {
-  const enteredNumber = getUserNumberInput();
-  const initialResult = currentResult;
-  currentResult /= enteredNumber;
-  createAndWriteOutput("/", initialResult, enteredNumber);
-  writeToLog("DIVIDE", initialResult, enteredNumber, currentResult);
-}
+document.getElementById("btn-multiply").onclick = () =>
+  calculate(Operations.Multiple);
 
-addBtn.addEventListener("click", add);
-subtractBtn.addEventListener("click", subtract);
-multiplyBtn.addEventListener("click", multiply);
-divideBtn.addEventListener("click", divide);
+document.getElementById("btn-divide").onclick = () =>
+  calculate(Operations.Divide);
