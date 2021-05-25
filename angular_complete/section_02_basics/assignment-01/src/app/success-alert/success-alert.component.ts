03dc39eded3c4e41376392848e7a680876749856
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2021-05-25 17:46:50 -0600

    first assignmnet updated

--- angular_complete/section_02_basics/assignment-01/src/app/success-alert/success-alert.component.ts
+++ angular_complete/section_02_basics/assignment-01/src/app/success-alert/success-alert.component.ts
@@ -2,6 +2,15 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "app-success-alert",
-  templateUrl: "success-alert.component.html",
+  template: `<p>Your Task Was Successful!</p>`,
+  styles: [
+    `
+      p {
+        padding: 20px;
+        background-color: lightgreen;
+        border: 1px solid green;
+      }
+    `,
+  ],
 })
 export class SuccessAlertComponent {}
