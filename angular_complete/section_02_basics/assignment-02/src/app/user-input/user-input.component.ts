6a264c3eb9806f336f3b4d2fa5d77aa0e5c7f5cf
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2021-05-26 12:01:25 -0600

    finished assignment 2 in section 2 of angular course

---
+++ angular_complete/section_02_basics/assignment-02/src/app/user-input/user-input.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "app-user-input",
+  templateUrl: "./user-input.component.html",
+  styleUrls: ["./user-input.component.css"],
+})
+export class UserInputComponent implements OnInit {
+  userName = "";
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onButtonClicked() {
+    this.userName = "";
+  }
+}
